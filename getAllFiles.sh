12c74e1803f52225bb2c381dd81834faf05d3e82
Karim <uvclu@student.kit.edu>
2015-05-29 16:29:32 +0200

    added JESUP/JESDOWN samples

--- getAllFiles.sh
+++ getAllFiles.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTbarH_M-125_13TeV_amcatnlo-pythia8-tauola/BoostedTTH_MiniAOD/150509_134128/0000/ /nfs/dust/cms/user/kelmorab/13TeV_ttHF/tth MC_aMCatNLO_TTH 9125 0.5085 93694 1 nominal
-./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132153/0000/ /nfs/dust/cms/user/kelmorab/13TeV_ttHF/ttbar MC_MadGraph_TTbar 2500 831.76 25446993 10 nominal
-./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTZJets_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132229/0000/ /nfs/dust/cms/user/kelmorab/13TeV_ttHF/ttZ MC_MadGraph_ttZ 2523 2.232 249275 4 nominal
-./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTWJets_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132213/0000/ /nfs/dust/cms/user/kelmorab/13TeV_ttHF/ttW MC_MadGraph_ttW 2524 1.152 246521 4 nominal
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTbarH_M-125_13TeV_amcatnlo-pythia8-tauola/BoostedTTH_MiniAOD/150509_134128/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/tth MC_aMCatNLO_TTH 9125 0.5085 93694 1 nominal
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132153/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttbar MC_MadGraph_TTbar 2500 831.76 25446993 10 nominal
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTZJets_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132229/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttZ MC_MadGraph_ttZ 2523 2.232 249275 4 nominal
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTWJets_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132213/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttW MC_MadGraph_ttW 2524 1.152 246521 4 nominal
+
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTbarH_M-125_13TeV_amcatnlo-pythia8-tauola/BoostedTTH_MiniAOD/150509_134128/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/tth MC_aMCatNLO_TTH 9125 0.5085 93694 1 JESUP
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132153/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttbar MC_MadGraph_TTbar 2500 831.76 25446993 10 JESUP
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTZJets_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132229/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttZ MC_MadGraph_ttZ 2523 2.232 249275 4 JESUP
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTWJets_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132213/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttW MC_MadGraph_ttW 2524 1.152 246521 4 JESUP
+
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTbarH_M-125_13TeV_amcatnlo-pythia8-tauola/BoostedTTH_MiniAOD/150509_134128/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/tth MC_aMCatNLO_TTH 9125 0.5085 93694 1 JESDOWN
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132153/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttbar MC_MadGraph_TTbar 2500 831.76 25446993 10 JESDOWN
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTZJets_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132229/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttZ MC_MadGraph_ttZ 2523 2.232 249275 4 JESDOWN
+./get-filenames.sh /pnfs/desy.de/cms/tier2/store/user/hmildner/TTWJets_Tune4C_13TeV-madgraph-tauola/BoostedTTH_MiniAOD/150509_132213/0000/ /nfs/dust/cms/user/kelmorab/13TeV_puID/ttW MC_MadGraph_ttW 2524 1.152 246521 4 JESDOWN
