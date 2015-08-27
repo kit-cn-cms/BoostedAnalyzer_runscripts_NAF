55c756d488f2dbc1b5be142a7b98004a7050e8af
Karim <uvclu@student.kit.edu>
2015-08-27 16:24:00 +0200

    right samples

--- execute.sh
+++ execute.sh
@@ -3,16 +3,16 @@
 . /etc/profile.d/modules.sh
 
 module use -a /afs/desy.de/group/cms/modulefiles/
-module load cmssw/slc6_amd64_gcc481
+module load cmssw/slc6_amd64_gcc491
 #. /usr/sge/default/common/settings.sh
 
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch/
-export SCRAM_ARCH=slc6_amd64_gcc481
+export SCRAM_ARCH=slc6_amd64_gcc491
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-cd /afs/desy.de/user/k/kelmorab/CMSSW_7_2_3/src
+cd /afs/desy.de/user/k/kelmorab/CMSSW_7_4_6_patch6/src
 eval `scram runtime -sh`
-cd /afs/desy.de/user/k/kelmorab/run2
+cd /afs/desy.de/user/k/kelmorab/run2Spring15
 # program to execute
-./$INPUTSCRIPT cmsRun /afs/desy.de/user/k/kelmorab/CMSSW_7_2_3/src/BoostedTTH/BoostedAnalyzer/test/PreSel_cfg.py
+./$INPUTSCRIPT cmsRun /afs/desy.de/user/k/kelmorab/CMSSW_7_4_6_patch6/src/BoostedTTH/BoostedAnalyzer/test/BaseLineSel_cfg.py
 
