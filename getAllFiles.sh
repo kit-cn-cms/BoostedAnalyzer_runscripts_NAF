5d15449b4f3a7cc70c2a8085732174d2ba550018
Karim <uvclu@student.kit.edu>
2015-08-31 11:04:24 +0200

    fixed paths

--- getAllFiles.sh
+++ getAllFiles.sh
@@ -7,7 +7,7 @@
 
 
 # samples WITH FatJets
-./get-filenames.sh "/pnfs/desy.de/cms/tier2/store/user/shwillia/TT_TuneCUETP8M1_13TeV-powheg-pythia8/BoostedTTH_MiniAOD/150731_155453/0000/*" /nfs/dust/cms/user/kelmorab/Spring15_Base20thJuly/ttbar MC_powheg_TTbar 2500 832.0 19899500 5 nominal
+./get-filenames.sh "/pnfs/desy.de/cms/tier2/store/user/shwillia/TT_TuneCUETP8M1_13TeV-powheg-pythia8/BoostedTTH_MiniAOD/150731_155453/*" /nfs/dust/cms/user/kelmorab/Spring15_Base31thAugust/ttbar MC_powheg_TTbar 2500 832.0 19899500 5 nominal
 
-./get-filenames.sh "/pnfs/desy.de/cms/tier2/store/user/shwillia/ttHTobb_M125_13TeV_powheg_pythia8/BoostedTTH_MiniAOD/150727_223648/0000/" /nfs/dust/cms/user/kelmorab/Spring15_Base20thJuly/tthbb MC_powheg_tthbb 9125 0.2934 3933404 1 nominal
+./get-filenames.sh "/pnfs/desy.de/cms/tier2/store/user/shwillia/ttHTobb_M125_13TeV_powheg_pythia8/BoostedTTH_MiniAOD/150727_223648/*" /nfs/dust/cms/user/kelmorab/Spring15_Base31thAugust/tthbb MC_powheg_tthbb 9125 0.2934 3933404 1 nominal
 
