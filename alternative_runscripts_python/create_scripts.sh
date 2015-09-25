f225b23771f239d942acc190e43dc2478f445446
Hannes Mildner <hannes.mildner@cern.ch>
2015-09-25 14:35:34 +0200

    added small example script

---
+++ alternative_runscripts_python/create_scripts.sh
@@ -0,0 +1,2 @@
+./get_filenames.py /pnfs/desy.de/cms/tier2/store/mc/RunIISpring15DR74/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9_ext3-v1/ . ttbar 500 832.0 95192472
+./get_filenames.py /nfs/dust/cms/user/kelmorab/MINIAOD/RunIISpring15DR74/ttHTobb_M125_13TeV_powheg_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/ . tth 500 0.2934 3933404
