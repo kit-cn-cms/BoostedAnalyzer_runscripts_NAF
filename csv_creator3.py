4a6b12a7afbeff0a13a1ca5177aecf837fb5c3df
Michael Wassmer <ufcyw@student.kit.edu>
2016-02-10 15:31:58 +0100

    new csv files and better csv_creator

--- csv_creator3.py
+++ csv_creator3.py
@@ -21,7 +21,7 @@ def get_type(dataset):
     print 'looking for datatype'
     tmp=os.popen("python das_client.py --limit=20 --query='dataset="+dataset+" | grep dataset.datatype'"+" --format=plain").read()
     tmp1=tmp.rstrip().split('\n')[3]
-    #print tmp1
+    print tmp1
     return str(tmp1)
 
 def get_generator(dataset):
@@ -69,24 +69,28 @@ samples = {'category': {'ttbar': {'name' : 'ttbar' , 'dataset_wildcard': '/TT_Tu
                         'st_tWchan': {'name' : 'st_tWchan' , 'dataset_wildcard': '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM', 'XS': 35.6, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
                         'stbar_tWchan': {'name' : 'st_tbarWchan' , 'dataset_wildcard': '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM', 'XS': 35.6, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
                         'DYJetsToLL': {'name' : 'DYJetsToLL' , 'dataset_wildcard': '/DYJetsToLL*50_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',                     'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : 'aMC'},
-                        'WJets': {'name' : 'WJets' , 'dataset_wildcard': '/WJetsToLNu*TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,        					 'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : 'notSpecified'}}}#,
+                        'WJets': {'name' : 'WJets' , 'dataset_wildcard': '/WJetsToLNu*TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,        					 'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : 'notSpecified'},
+                        'el_data': {'name' : 'el_reminiaod' , 'dataset_wildcard': '/SingleElectron/Run2015D-16Dec2015-v1/MINIAOD' , 'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : '?'},
+                        'mu_data': {'name' : 'mu_reminiaod' , 'dataset_wildcard': '/SingleMuon/Run2015D-16Dec2015-v1/MINIAOD' , 'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : '?'}}}#,
                         #'QCD':   {'name' : 'QCD' , 'dataset_wildcard': '/QCD_HT*TuneCUETP8M1_13TeV*pythia8/RunIISpring15MiniAOD*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,        	                         'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?'}}}
 
 
 
-globalTag='76X_mcRun2_asymptotic_v12'
+globalTag_mc='76X_mcRun2_asymptotic_v12'
+globalTag_data='76X_dataRun2_v15'
 #categorys=samples['category']
 #print categorys
 #category=categorys['ttbar']
 #print category
 fobj_out = open("mc_samples.csv","w")
 for c in samples['category']:
+    fobj_out.write(',,,,,,,,'+'\n')
     categorys=samples['category']
     category=categorys[c]
     dataset=get_datasets(category['dataset_wildcard'])
     XS=category['XS'] #in pb
     NposmNnegoNtot=category['NposmNnegoNtot']
-    boosted_dataset=category['boosted_dataset']
+    #boosted_dataset=category['boosted_dataset']
     #generator=category['generator']
     #boosted_dataset_wildcard=get_boosted_datasets(category['boosted_dataset'])
     for s in dataset:
@@ -95,17 +99,19 @@ for c in samples['category']:
         generator=get_generator(s)
         data_or_mc=get_type(s)
         boosted_dataset1=get_boosted(s)
-        if(data_or_mc=='"mc"'):
-             true_or_false='FALSE'
-        if(data_or_mc=="data"):
-             true_or_false='TRUE'
-        print 'is data? ',true_or_false
         if(NposmNnegoNtot==0):
              weights=XS/(float(nevents)*1)*1000
         else:
              weights=XS/(float(nevents)*NposmNnegoNtot)*1000
         print 'writing to file'
-        fobj_out.write(c+','+str(s)+','+nevents+','+str(NposmNnegoNtot)+','+str(XS)+','+str(weights)+','+str(boosted_dataset1).replace("'","").replace(","," | ")+','+globalTag+','+true_or_false+','+generator+'\n')
+        if(data_or_mc=='"mc"'):
+             true_or_false='FALSE'
+             fobj_out.write(c+','+str(s)+','+nevents+','+str(NposmNnegoNtot)+','+str(XS)+','+str(weights)+','+str(boosted_dataset1).replace("'","").replace(","," | ")	  		     +','+globalTag_mc+','+true_or_false+','+generator+'\n')
+        if(data_or_mc=='"data"'):
+             true_or_false='TRUE'
+	     fobj_out.write(c+','+str(s)+','+nevents+',,,,'+str(boosted_dataset1).replace("'","").replace(","," | ")+','+globalTag_data+','+true_or_false+','+'\n')
+        
+        
         #print c+','+str(s)+','+str(nevents)+',Npos-Nneg/Ntotal,'+str(XS)+','+str(weights)+',boosted_dataset,'+globalTag+',FALSE'
 fobj_out.close()
 
