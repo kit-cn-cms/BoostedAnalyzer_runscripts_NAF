390a0de1b283e1d896952c71dae5823cb6c7e4e8
Michael Wassmer <ufcyw@student.kit.edu>
2016-02-12 14:47:25 +0100

    now recognizes amc datasets and counts the appropriate scale factor for the weights

--- csv_creator3.py
+++ csv_creator3.py
@@ -3,6 +3,69 @@
 import das_client
 import os
 import subprocess
+import re
+import ROOT
+
+########################
+def GetTotalSampleNumbers(file_list):
+	files=file_list
+	usexroot=False
+	for f in files:
+	  if f=="-x":
+	   usexroot=True
+	   files.remove("-x")
+	   break
+	print files
+
+
+
+	totalNumber=0
+	totalPos=0
+	totalNeg=0
+	#hnE=ROOT.TH1F("hnE","hnE",100,-10,10)
+	FileList=[]
+	  
+	FileList=files
+
+	print "N files ", len(FileList)
+	nfiles=len(FileList)
+	print "counting events"
+	ifile=0
+	##write files where the tree could not be found in this file
+	blacklist=open("blacklist.txt","w")
+	##now count the events
+	for f in FileList:
+	  print f
+	  
+	  tf=ROOT.TFile(str(f),"READ")
+	  tree=tf.Get("Events")
+	  if tree==None:
+	    blacklist.write(f+"\n")
+	    continue
+	  hpE=ROOT.TH1D("hpE","hpE",100,-10,10)
+	  hnE=ROOT.TH1D("hnE","hnE",100,-10,10)
+	  tree.Project("hpE","GenEventInfoProduct_generator__SIM.obj.weights_","GenEventInfoProduct_generator__SIM.obj.weights_>0")
+	  tree.Project("hnE","GenEventInfoProduct_generator__SIM.obj.weights_","GenEventInfoProduct_generator__SIM.obj.weights_<0")
+	  np=hpE.GetEntries()
+	  nn=hnE.GetEntries()
+	  print "done with File ", ifile, "/", nfiles, f
+	  print np, nn
+	  totalPos+=np
+	  totalNeg+=nn
+	  tf.Close()
+	  ifile+=1
+
+	blacklist.close()
+
+	totalNumber=totalPos-totalNeg
+	print "total number of positive events: "+str(int(totalPos))
+	print "total number of negative events: "+str(int(totalNeg))
+	print "total number of events: "+str(int(totalNumber))
+	print "check the blacklist.txt file for problematic input samples."
+	print "get-filenames.sh will ignore files in this list."
+        print 'x=',(totalPos-totalNeg)/(totalPos+totalNeg)
+        return (totalPos-totalNeg)/(totalPos+totalNeg)
+##################
 
 def get_datasets(dataset_wildcard):
     print 'getting datasets of ',dataset_wildcard
@@ -57,23 +120,41 @@ def get_boosted(parent_dataset):
          print tmp1[0]
          return tmp1[0]
 
-samples = {'category': {'ttbar': {'name' : 'ttbar' , 'dataset_wildcard': '/TT_TuneCUETP8M1_13TeV*powheg-pythia8*/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,        					  'XS': 831.76, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/*Boostedv4MiniAOD*/USER' , 'generator' : 'POWHEG'},
-			'ttHbb': {'name' : 'ttHbb' , 'dataset_wildcard': '/ttHTobb_M125_13TeV*powheg_pythia8*/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' , 
-                                  'XS': 0.2934, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '/ttHTobb_M125_13TeV_powheg_pythia8/*Boostedv4MiniAOD*/USER' , 'generator' : 'POWHEG'},
-                        'ttHnonbb' : {'name' : 'ttHnonbb' , 'dataset_wildcard': '/ttHToNonbb_M125_13TeV*powheg_pythia8*/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' , 
-                                  'XS': 0.2151, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '/ttHToNonbb_M125_13TeV_powheg_pythia8/*Boostedv4MiniAOD*/USER' , 'generator' : 'POWHEG'},
-                        'ZZ' : {'name' : 'ZZ' , 'dataset_wildcard':  '/ZZ_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',
-                                  'XS': 16.523, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'pythia8'},
-                        'WZ' : {'name' : 'WZ' , 'dataset_wildcard':  '/WZ_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',
-                                  'XS': 47.13, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'pythia8'},
-                        'WW' : {'name' : 'WW' , 'dataset_wildcard':  '/WW_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',
-                                  'XS': 118.7, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'pythia8'},
-                        'st_tchan': {'name' : 'st_chan' , 'dataset_wildcard': '/ST_t-channel_top_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,'XS': 45.3, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
-                        'stbar_tchan': {'name' : 'stbar_chan' , 'dataset_wildcard': '/ST_t-channel_antitop_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,'XS': 27.0, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
-                        'st_tWchan': {'name' : 'st_tWchan' , 'dataset_wildcard': '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM', 'XS': 35.6, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
-                        'stbar_tWchan': {'name' : 'st_tbarWchan' , 'dataset_wildcard': '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM', 'XS': 35.6, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
-                        'DYJetsToLL': {'name' : 'DYJetsToLL' , 'dataset_wildcard': '/DYJetsToLL*50_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',                     'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : 'aMC'},
-                        'WJets': {'name' : 'WJets' , 'dataset_wildcard': '/WJetsToLNu*TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,        					 'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : 'notSpecified'},
+def get_nfiles(dataset):
+    print 'requesting nfiles'
+    tmp=os.popen("python das_client.py --limit=20 --query='dataset="+dataset+" | grep dataset.nfiles'"+" --format=plain").read()
+    tmp1=tmp.rstrip().split('\n')[3]
+    print tmp1
+    return tmp1
+
+def get_store(dataset,nfiles):
+    print 'looking for store place on naf'
+    tmp=os.popen("python das_client.py --limit="+str(nfiles)+" --query='file dataset="+dataset+" instance=prod/phys03"+" | grep file.name'"+" --format=plain").read()
+    tmp1=tmp.replace('"','').replace("/store","/pnfs/desy.de/cms/tier2/store")
+    tmp2=tmp1.rstrip().split('\n')[3:]
+    #print str(tmp1).replace("', '",' ').replace("['","").replace('"','').replace("']","")
+    #return str(tmp1).replace("', '",' ').replace("['","").replace('"','').replace("']","").replace("/store","/pfns/desy.de/cms/tier2/store")
+    print tmp2
+    return tmp2
+
+samples = {'category': {#'ttbar': {'name' : 'ttbar' , 'dataset_wildcard': '/TT_TuneCUETP8M1_13TeV*powheg-pythia8*/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,        					  'XS': 831.76, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/*Boostedv4MiniAOD*/USER' , 'generator' : 'POWHEG'},
+			#'ttHbb': {'name' : 'ttHbb' , 'dataset_wildcard': '/ttHTobb_M125_13TeV*powheg_pythia8*/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' , 
+                        #          'XS': 0.2934, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '/ttHTobb_M125_13TeV_powheg_pythia8/*Boostedv4MiniAOD*/USER' , 'generator' : 'POWHEG'},
+                        #'ttHnonbb' : {'name' : 'ttHnonbb' , 'dataset_wildcard': '/ttHToNonbb_M125_13TeV*powheg_pythia8*/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' , 
+                        #          'XS': 0.2151, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '/ttHToNonbb_M125_13TeV_powheg_pythia8/*Boostedv4MiniAOD*/USER' , 'generator' : 'POWHEG'},
+                        #'ZZ' : {'name' : 'ZZ' , 'dataset_wildcard':  '/ZZ_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',
+                        #          'XS': 16.523, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'pythia8'},
+                        #'WZ' : {'name' : 'WZ' , 'dataset_wildcard':  '/WZ_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',
+                        #          'XS': 47.13, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'pythia8'},
+                        #'WW' : {'name' : 'WW' , 'dataset_wildcard':  '/WW_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',
+                        #          'XS': 118.7, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'pythia8'},
+                        #'st_tchan': {'name' : 'st_chan' , 'dataset_wildcard': '/ST_t-channel_top_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,'XS': 45.3, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
+                        #'stbar_tchan': {'name' : 'stbar_chan' , 'dataset_wildcard': '/ST_t-channel_antitop_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,'XS': 27.0, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
+                        #'st_tWchan': {'name' : 'st_tWchan' , 'dataset_wildcard': '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM', 'XS': 35.6, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
+                        #'stbar_tWchan': {'name' : 'st_tbarWchan' , 'dataset_wildcard': '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM', 'XS': 35.6, 'NposmNnegoNtot' : 1, 'boosted_dataset' : '?' , 'generator' : 'POWHEG'},
+                        'DYJetsToLL1': {'name' : 'Zjets_m10to50' , 'dataset_wildcard': '/DYJetsToLL*10to50_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',                     'XS': 3*7545.03, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : 'aMC'},
+                        'DYJetsToLL2': {'name' : 'Zjets_m50toInf' , 'dataset_wildcard': '/DYJetsToLL*-50_TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM',                     'XS': 3*2008.4, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : 'aMC'},
+                        #'WJets': {'name' : 'WJets' , 'dataset_wildcard': '/WJetsToLNu*TuneCUETP8M1_13TeV*pythia8/RunIIFall15MiniAODv2*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,        					 'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : 'notSpecified'},
                         'el_data': {'name' : 'el_reminiaod' , 'dataset_wildcard': '/SingleElectron/Run2015D-16Dec2015-v1/MINIAOD' , 'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : '?'},
                         'mu_data': {'name' : 'mu_reminiaod' , 'dataset_wildcard': '/SingleMuon/Run2015D-16Dec2015-v1/MINIAOD' , 'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?' , 'generator' : '?'}}}#,
                         #'QCD':   {'name' : 'QCD' , 'dataset_wildcard': '/QCD_HT*TuneCUETP8M1_13TeV*pythia8/RunIISpring15MiniAOD*76X_mcRun2_asymptotic_v12*/MINIAODSIM' ,        	                         'XS': 0, 'NposmNnegoNtot' : 0, 'boosted_dataset' : '?'}}}
@@ -106,7 +187,10 @@ for c in samples['category']:
         if(generator.find("amc")==-1 and generator.find("aMC")==-1):
              NposmNnegoNtot=1
         else:
-             NposmNnegoNtot=0
+             nfiles=get_nfiles(s)
+             dataset_store=get_store(boosted_dataset1,nfiles)
+             NposmNnegoNtot=GetTotalSampleNumbers(dataset_store)
+             print 'scale factor',NposmNnegoNtot
         if(NposmNnegoNtot==0):
              weights=XS/(float(nevents)*1)*1000
         else:
