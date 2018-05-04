22821ae0cd57e37d08371014f76c83820c8cdbe2
Jan van der Linden <jan.linden@student.kit.edu>
2018-05-04 12:08:34 +0200

    update sup.py to HTCsubmit

--- alternative_runscripts_python/sup.py
+++ alternative_runscripts_python/sup.py
@@ -3,13 +3,15 @@
 # usage: ./sup.py path/to/scripts [script_pattern]
 import os
 import sys
+from subprocess import call
+
 pattern=''
 path=''
+
 if len(sys.argv) > 1:
     path= sys.argv[1]+'/'
 if len(sys.argv) > 2:
     pattern= sys.argv[2]
-from subprocess import call
 
 if not os.path.exists('logs'):
     os.makedirs('logs')
@@ -20,4 +22,25 @@ files = [os.path.join(root, name)
                      if pattern in name and name.endswith((".sh"))]
 for f in files:
 #    print f
-    call(['qsub', '-cwd', '-S', '/bin/bash','-l', 'h=bird*', '-hard','-l', 'os=sld6', '-l' ,'h_vmem=2000M', '-l', 's_vmem=2000M' ,'-o', 'logs/$JOB_NAME.o$JOB_ID', '-e', 'logs/$JOB_NAME.e$JOB_ID', f])
+    filename = f.split("/")[-1][:-3]
+    # writing submit script file
+    pathSubmitScript = path + "/submitFile_"+filename+".sub"
+    codeSubmitScript = "universe = vanilla\n"
+    codeSubmitScript += "should_transfer_files = IF_NEEDED\n"
+    codeSubmitScript += "executable = /bin/bash\n"
+    codeSubmitScript += "arguments = " + f + "\n"
+    codeSubmitScript += "error = logs/" + filename + "_$(Cluster).out\n"
+    codeSubmitScript += "output = logs/" + filename + "_$(Cluster).err\n"
+    codeSubmitScript += "notification = Never\n"
+    codeSubmitScript += "priority = 0\n"
+    codeSubmitScript += "request_memory = 2000M\n"
+    #codeSubmitScript += "request_disk = 2000M\n"
+    codeSubmitScript += "queue"
+
+    fileSubmitScript = open(pathSubmitScript, "w")
+    fileSubmitScript.write(codeSubmitScript)
+    fileSubmitScript.close()
+
+    print "submitting script", f
+    command = "condor_submit " + pathSubmitScript
+    call(command.split())
