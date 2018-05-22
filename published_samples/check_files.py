17089d02a569a2b9a3ac516aabb889653e02b4c1
Michael Wassmer <michael.wassmer@cern.ch>
2018-05-22 10:11:12 +0200

    save changes in check files

--- published_samples/check_files.py
+++ published_samples/check_files.py
@@ -44,8 +44,8 @@ def get_splitting_of_systematics(systematics,nvariations):
 
 
 files_path="/nfs/dust/cms/user/mwassmer/DarkMatter/ntuples/"
-scripts_path="/nfs/dust/cms/user/mwassmer/DarkMatter/BoostedAnalyzer_runscripts_NAF/published_samples/MonoJet/"
-nsystematicvariations=2 #specifiy how many variations one job used, needs to be the same which was used in generate scripts
+scripts_path="/nfs/dust/cms/user/mwassmer/DarkMatter/BoostedAnalyzer_runscripts_NAF/published_samples/MonoJet_skims_alternative/"
+nsystematicvariations=5 #specifiy how many variations one job used, needs to be the same which was used in generate scripts
 
 #use this code snippet to find the samples in the scripts path automatically
 
@@ -57,20 +57,10 @@ for i in range(len(samples)):
 #print samples
 
 # or check for specific datasets
-#samples=["st_schan_amc_pythia_CUETP8M1","st_tWchan_powheg_pythia_CUETP8M1","st_tWchan_powheg_pythia_CUETP8M2T4","st_tchan_powheg_herwig","st_tchan_powheg_pythia_CUETP8M1","st_tchan_powheg_pythia_CUETP8M2T4","stbar_tWchan_powheg_pythia_CUETP8M1","stbar_tWchan_powheg_pythia_CUETP8M2T4","stbar_tchan_powheg_herwig","stbar_tchan_powheg_pythia_CUETP8M1","stbar_tchan_powheg_pythia_CUETP8M2T4"]
-#samples=["Wjets-HT-70-100_madgraph_pythia_CUETP8M1","Wjets-HT-100-200_madgraph_pythia_CUETP8M1","Wjets-HT-200-400_madgraph_pythia_CUETP8M1","Wjets-HT-400-600_madgraph_pythia_CUETP8M1","Wjets-HT-600-800_madgraph_pythia_CUETP8M1","Wjets-HT-800-1200_madgraph_pythia_CUETP8M1","Wjets-HT-1200-2500_madgraph_pythia_CUETP8M1","Wjets-HT-2500-Inf_madgraph_pythia_CUETP8M1"]
-#samples=["WW_pythia_CUETP8M1","WZ_pythia_CUETP8M1","ZZ_pythia_CUETP8M1"]
-#samples=["ttW_hadr_amc_pythia_CUETP8M1","ttW_lept_amc_pythia_CUETP8M1","ttZ_hadr_amc_pythia_CUETP8M1","ttZ_lept_amc_pythia_CUETP8M1"]
-#samples=["Zjets_m5to50-HT-70-100_madgraph_pythia_CUETP8M1","Zjets_m5to50-HT-100-200_madgraph_pythia_CUETP8M1","Zjets_m5to50-HT-200-400_madgraph_pythia_CUETP8M1","Zjets_m5to50-HT-400-600_madgraph_pythia_CUETP8M1","Zjets_m5to50-HT-600-Inf_madgraph_pythia_CUETP8M1"]
-#samples=["Zjets_m50toInf-HT-70-100_madgraph_pythia_CUETP8M1","Zjets_m50toInf-HT-100-200_madgraph_pythia_CUETP8M1","Zjets_m50toInf-HT-200-400_madgraph_pythia_CUETP8M1","Zjets_m50toInf-HT-400-600_madgraph_pythia_CUETP8M1","Zjets_m50toInf-HT-600-800_madgraph_pythia_CUETP8M1","Zjets_m50toInf-HT-800-1200_madgraph_pythia_CUETP8M1","Zjets_m50toInf-HT-1200-2500_madgraph_pythia_CUETP8M1","Zjets_m50toInf-HT-2500-Inf_madgraph_pythia_CUETP8M1"]
-#samples=["SingleElectron_Run2016B","SingleElectron_Run2016C","SingleElectron_Run2016D","SingleElectron_Run2016E","SingleElectron_Run2016F","SingleElectron_Run2016G","SingleElectron_Run2016Hv2","SingleElectron_Run2016Hv3","SingleMuon_Run2016B","SingleMuon_Run2016C","SingleMuon_Run2016D","SingleMuon_Run2016E","SingleMuon_Run2016F","SingleMuon_Run2016G","SingleMuon_Run2016Hv2","SingleMuon_Run2016Hv3"]
-#samples=["ttbb_amc","ttbb_sherpa_ol"]
-#samples=["ttbar_isr_up","ttbar_isr_down","ttbar_fsr_up","ttbar_fsr_down","ttbar_ue_up","ttbar_ue_down","ttbar_hdamp_up","ttbar_hdamp_down","ttbar_color_reco"]
-#samples=["ttbar_mtop1665","ttbar_mtop1695","ttbar_mtop1715","ttbar_mtop1735","ttbar_mtop1755","ttbar_mtop1785"]
-#samples=["QCD_HT50to100","QCD_HT100to200","QCD_HT200to300","QCD_HT300to500","QCD_HT500to700","QCD_HT700to1000","QCD_HT1000to1500","QCD_HT1500to2000","QCD_HT2000toInf"]
-samples=[sample for sample in samples if sample.find("DMV_NNPDF30_Axial")!=-1]
-
-#systematics=get_list_of_systematics("systematicVariations.txt")
+#samples=["WJetsToLNu_Pt-100To250_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8","WJetsToLNu_Pt-250To400_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8","WJetsToLNu_Pt-400To600_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8","WJetsToLNu_Pt-600ToInf_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8"]
+samples=[sample for sample in samples if sample.find("MET")!=-1]
+print samples
+#systematics=get_list_of_systematics("systematicVariations_used.txt")
 #systematics_,systematics_numbers=get_splitting_of_systematics(systematics,nsystematicvariations)	
 
 #use in case of data since there are no systematic variations
