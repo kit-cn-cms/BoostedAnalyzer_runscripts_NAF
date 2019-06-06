105d51f82440c6e649d70a20952c3188cbec9db0
Jan van der Linden <jan.linden@student.kit.edu>
2019-06-06 13:01:29 +0200

    configs for ttZ ntuples

--- published_samples/user_config_legacy_2018_Jan.py
+++ published_samples/user_config_legacy_2018_Jan.py
@@ -1,10 +1,10 @@
-outpath='/nfs/dust/cms/user/vdlinden/legacyTTH/ntuples/DNN_ntuples_v1/' # path of output of analyzer
-scriptpath='legacy_2018_dnn' # folder containing shell scripts that will have to be run on cluster
-samplelist='legacy_2018_nominal_dnn.csv' # samples list
-dataset_column='dataset' # run on the column with dataset or boosted_dataset?
+outpath='/nfs/dust/cms/user/vdlinden/legacyTTH/ntuples/2018_ttZ/' # path of output of analyzer
+scriptpath='legacy_2018_ttZ' # folder containing shell scripts that will have to be run on cluster
+samplelist='../ttH_legacy_samples_2018_ttZ_Jan.csv' # samples list
+dataset_column='boosted_dataset' # run on the column with dataset or boosted_dataset?
 cmsswcfgpath='/nfs/dust/cms/user/vdlinden/legacyTTH/CMSSW/CMSSW_10_2_13/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
 cmsswpath='/nfs/dust/cms/user/vdlinden/legacyTTH/CMSSW/CMSSW_10_2_13/'
-dbs="prod/global" # dbs instance: boosted miniaod is in prod/phys03, standard miniaod in prod/global
+dbs="prod/phys3" # dbs instance: boosted miniaod is in prod/phys03, standard miniaod in prod/global
 min_events_per_job=100000 # min number of events per job 
 isBoostedMiniAOD=False # do the inputs contain fat jets?
 systematicVariations='noSysts.txt'
