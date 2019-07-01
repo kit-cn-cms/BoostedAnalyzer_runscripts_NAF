83e15f33769fe0e7aa6faca98d2bbdcb25b73052
sebwieland <wielandseb@googlemail.com>
2019-07-01 08:43:20 +0200

    added configs for legacy ntuples

---
+++ configs/user_config_legacy_2018.py
@@ -0,0 +1,12 @@
+outpath='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple/2018' # path of output of analyzer
+scriptpath='legacy_2018' # folder containing shell scripts that will have to be run on cluster
+samplelist='ttH_legacy_samples_2018.csv' # samples list
+dataset_column='boosted_dataset' # run on the column with dataset or boosted_dataset?
+cmsswcfgpath='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple/CMSSW_10_2_13/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
+cmsswpath='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple/CMSSW_10_2_13/'
+dbs="prod/phys03" # dbs instance: boosted miniaod is in prod/phys03, standard miniaod in prod/global
+min_events_per_job=100000 # min number of events per job 
+isBoostedMiniAOD=False # do the inputs contain fat jets?
+systematicVariations='totalJECSysts.txt'
+nSystematicVariationsPerJob=3
+ProduceMemNtuples=False
