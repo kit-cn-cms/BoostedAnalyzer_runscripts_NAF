bf95c6f1523697ba971c9036850d02ce27a12763
Jan van der Linden <jan.linden@student.kit.edu>
2019-07-08 13:34:38 +0200

    update configs ttZ18

--- published_samples/user_config_legacy_2018_Jan.py
+++ published_samples/user_config_legacy_2018_Jan.py
@@ -1,6 +1,6 @@
 outpath='/nfs/dust/cms/user/vdlinden/legacyTTH/ntuples/legacy_2018_ttZ_v2/' # path of output of analyzer
-scriptpath='legacy_2018_ttZ_v2' # folder containing shell scripts that will have to be run on cluster
-samplelist='../ttH_legacy_samples_2018_ttZ_Jan.csv' # samples list
+scriptpath='legacy_2018_ttZ_v2_data' # folder containing shell scripts that will have to be run on cluster
+samplelist='../ttH_legacy_samples_2018_ttZ_Jan_data.csv' # samples list
 dataset_column='boosted_dataset' # run on the column with dataset or boosted_dataset?
 cmsswcfgpath='/nfs/dust/cms/user/vdlinden/legacyTTH/CMSSW/CMSSW_10_2_13/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
 cmsswpath='/nfs/dust/cms/user/vdlinden/legacyTTH/CMSSW/CMSSW_10_2_13/'
