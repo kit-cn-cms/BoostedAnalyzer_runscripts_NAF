dc5f7e4507e6b9a8c18921fa639a322a416a46a9
Jan van der Linden <jan.linden@student.kit.edu>
2019-08-30 16:24:31 +0200

    remove trash

--- runProblems.sh
+++
@@ -1,6 +0,0 @@
-for i in `cat output/sampleListe_Problems.txt`
-  do 
-  INPUTSCRIPT=$i
-  export INPUTSCRIPT 
-  qsub -l h=bird* -hard -l os=sld6 -l h_vmem=2000M -l s_vmem=2000M -cwd -S /bin/bash -v INPUTSCRIPT -N `basename $i .sh` -o logs/\$JOB_NAME.o\$JOB_ID -e logs/\$JOB_NAME.e\$JOB_ID -q 'default.q' execute.sh
-done
