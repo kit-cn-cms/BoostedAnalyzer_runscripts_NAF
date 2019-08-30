68b70a68fe4541efa355e073fdeb8bafa2276f85
Jan van der Linden <jan.linden@student.kit.edu>
2019-08-30 19:06:11 +0200

    new sample list and configs

---
+++ published_samples/config_ttZAnalysis_ttZqq.py
@@ -0,0 +1,12 @@
+outpath='/nfs/dust/cms/user/vdlinden/ttZAnalysis/' # path of output of analyzer
+scriptpath='ttZAnalysis_ttZqq' # folder containing shell scripts that will have to be run on cluster
+samplelist='../samples_ttZAnalysis_ttZqq.csv' # samples list
+dataset_column='dataset' # run on the column with dataset or boosted_dataset?
+cmsswcfgpath='/nfs/dust/cms/user/vdlinden/ttZAnalysis/CMSSW_ntupling/CMSSW_10_2_13/src/BoostedTTH/BoostedAnalyzer/test/ntupling_ttZAnalysis.py'
+cmsswpath='/nfs/dust/cms/user/vdlinden/ttZAnalysis/CMSSW_ntupling/CMSSW_10_2_13/'
+dbs="prod/global" # dbs instance: boosted miniaod is in prod/phys03, standard miniaod in prod/global
+min_events_per_job=100000 # min number of events per job 
+isBoostedMiniAOD=False # do the inputs contain fat jets?
+systematicVariations='systematics_ttZAnalysis.txt'
+nSystematicVariationsPerJob=3
+ProduceMemNtuples=False
