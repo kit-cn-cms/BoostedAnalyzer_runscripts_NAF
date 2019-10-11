db143e539aa6d571eff51a53a395e72747967969
sebwieland <wielandseb@googlemail.com>
2019-10-11 14:27:08 +0200

    added lists without wrong pu samples

--- configs/user_config_legacy_2017.py
+++ configs/user_config_legacy_2017.py
@@ -1,6 +1,6 @@
 outpath='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_v2/2017' # path of output of analyzer
 scriptpath='legacy_2017_v2' # folder containing shell scripts that will have to be run on cluster
-samplelist='ttH_legacy_samples_2017.csv' # samples list
+samplelist='ttH_legacy_samples_2017_incomplete.csv' # samples list
 dataset_column='boosted_dataset' # run on the column with dataset or boosted_dataset?
 cmsswcfgpath='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_v2/CMSSW_9_4_13/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
 cmsswpath='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_v2/CMSSW_9_4_13/'
