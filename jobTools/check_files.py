6e4bb79a555c37350cf9a5343048484b9a8f9534
sebwieland <wielandseb@googlemail.com>
2019-10-31 15:54:30 +0100

    updated check_files to latest version from michael

--- jobTools/check_files.py
+++ jobTools/check_files.py
@@ -112,8 +112,6 @@ print systematics_
 print "\n"
 #print systematics
 
-filestoResubmit = []
-
 for sample in samples:
     print "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------"
     print ""
@@ -182,11 +180,6 @@ for sample in samples:
 				missing_files+=" "+os.path.join(scripts_path,sample)+"/"+sample+"_"+number+"_"+str(systematic_number)+".sh"
                 
 		print "Have to resubmit ", n_missing_scripts, " scripts."
-		filestoResubmit.append(missing_files)
-    	        if options.resubmit:
+                if options.resubmit:
                     print "resubmitting ", n_missing_scripts, " scripts ..."
                     os.system("python submit.py "+missing_files)
-
-		with open("filesToResubmit.txt","w") as file:
-			for missingJob in filestoResubmit:
-				file.write(missingJob+"\n")
