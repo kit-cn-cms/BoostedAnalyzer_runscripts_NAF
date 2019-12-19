98cd1d0b7055369cef510e3efa3d6a8bdf5333ca
sebwieland <wielandseb@googlemail.com>
2019-12-19 15:53:34 +0100

    updates on systematics

--- configs/user_config_legacy_2018.py
+++ configs/user_config_legacy_2018.py
@@ -1,6 +1,6 @@
 outpath='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_JECgroups/2018' # path of output of analyzer
 scriptpath='legacy_2018' # folder containing shell scripts that will have to be run on cluster
-samplelist='ttH_legacy_samples_2018.csv' # samples list
+samplelist='ttH_legacy_samples_2018_Sebastian.csv' # samples list
 dataset_column='boosted_dataset' # run on the column with dataset or boosted_dataset?
 
 cmsswcfgpathSL6='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_JECgroups/CMSSW_10_2_18/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
@@ -13,5 +13,5 @@ dbs="prod/phys03" # dbs instance: boosted miniaod is in prod/phys03, standard mi
 min_events_per_job=100000 # min number of events per job 
 isBoostedMiniAOD=False # do the inputs contain fat jets?
 systematicVariations='systematicVariations_new_2018.txt'
-nSystematicVariationsPerJob=3
+nSystematicVariationsPerJob=4
 ProduceMemNtuples=False
