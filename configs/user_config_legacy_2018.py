f0791bfe070a117d8cf1e503ef1dc00e47462261
sebwieland <wielandseb@googlemail.com>
2019-12-19 15:58:08 +0100

    done be so selfish swieland

--- configs/user_config_legacy_2018.py
+++ configs/user_config_legacy_2018.py
@@ -1,6 +1,6 @@
 outpath='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_JECgroups/2018' # path of output of analyzer
 scriptpath='legacy_2018' # folder containing shell scripts that will have to be run on cluster
-samplelist='ttH_legacy_samples_2018_Sebastian.csv' # samples list
+samplelist='ttH_legacy_samples_2018.csv' # samples list
 dataset_column='boosted_dataset' # run on the column with dataset or boosted_dataset?
 
 cmsswcfgpathSL6='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_JECgroups/CMSSW_10_2_18/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
