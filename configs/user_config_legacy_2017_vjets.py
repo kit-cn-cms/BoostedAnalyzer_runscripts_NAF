1824ddca0eb1cc4173e09853baa2204378e63a49
Jan van der Linden <jan.linden@student.kit.edu>
2020-06-08 10:21:01 +0200

    vjets LO samples

---
+++ configs/user_config_legacy_2017_vjets.py
@@ -0,0 +1,17 @@
+outpath='/nfs/dust/cms/user/vdlinden/legacyTTH/ntupling/2017_vjets' # path of output of analyzer
+scriptpath='legacy_2017_vjets' # folder containing shell scripts that will have to be run on cluster
+samplelist='ttH_legacy_lo_vjets_2017.csv' # samples list
+dataset_column='boosted_dataset' # run on the column with dataset or boosted_dataset?
+
+cmsswcfgpathSL6='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_JECgroups/CMSSW_10_2_18/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
+cmsswpathSL6='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_JECgroups/CMSSW_10_2_18/'
+
+cmsswcfgpathSL7='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_JECgroups/CC7/CMSSW_10_2_18/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
+cmsswpathSL7='/nfs/dust/cms/user/swieland/ttH_legacy/ntuple_JECgroups/CC7/CMSSW_10_2_18/'
+
+dbs="prod/phys03" # dbs instance: boosted miniaod is in prod/phys03, standard miniaod in prod/global
+min_events_per_job=100000 # min number of events per job 
+isBoostedMiniAOD=False # do the inputs contain fat jets?
+systematicVariations='noSysts.txt'
+nSystematicVariationsPerJob=3
+ProduceMemNtuples=False
