e09ef03718af84a173bd33758ae5e3c987c8f8b6
Michael Wassmer <michael.wassmer@cern.ch>
2020-07-09 11:58:34 +0200

    save 2018 user config

---
+++ published_samples/user_config_Monotop_2018.py
@@ -0,0 +1,12 @@
+outpath='/nfs/dust/cms/user/mwassmer/MonoTop/ntuples_2018/' # path of output of analyzer
+scriptpath='MonoTop_2018' # folder containing shell scripts that will have to be run on cluster
+samplelist='../auto_samples_datasets_monotop_2018_v5.csv' # samples list
+dataset_column='boosted_dataset' # run on the column with dataset or boosted_dataset?
+cmsswcfgpath='/nfs/dust/cms/user/mwassmer/MonoTop/test/slc7/CMSSW_10_2_18/src/BoostedTTH/BoostedAnalyzer/test/boostedAnalysis_ntuples-Legacy_2016_2017_2018_cfg.py'
+cmsswpath='/nfs/dust/cms/user/mwassmer/MonoTop/test/slc7/CMSSW_10_2_18/'
+dbs="prod/phys03" # dbs instance: boosted miniaod is in prod/phys03, standard miniaod in prod/global
+min_events_per_job=100000 # min number of events per job 
+isBoostedMiniAOD=False # do the inputs contain fat jets?
+systematicVariations='systematicVariations_no_sources.txt'
+nSystematicVariationsPerJob=4
+ProduceMemNtuples=False
